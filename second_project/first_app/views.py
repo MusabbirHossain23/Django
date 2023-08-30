52065be0544f73edb9fac7d15ca25691e2697594
MusabbirHossain23 <murad.bnmpc2351@gmail.com>
2023-08-31 00:23:31 +0600

    first commit

---
+++ second_project/first_app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+def contact(request):
+    return HttpResponse('''
+                        
+                       <h1>This is contact page<h1>
+                       <a href='/first_app/about/'>About</a>
+                       <a href='/second_app/courses/'>courses</a>
+                       <a href='/second_app/feedback/'>feedback</a>
+                       
+                       ''')
+
+def  about(request):
+    return HttpResponse('''
+                        
+                       <h1>This is about page<h1>
+                       <a href='/first_app/contact/'>contact</a>
+                       <a href='/second_app/courses/'>courses</a>
+                       <a href='/second_app/feedback/'>feedback</a>
+                       
+                       ''')
